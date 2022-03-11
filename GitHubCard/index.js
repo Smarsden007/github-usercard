6d2bd1d2b9574ae5861bc575a5541339e57ff715
Sean Marsden <sean@Seans-MacBook-Pro.local>
2022-03-10 20:32:40 -0800

    adding element in index

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,16 @@
+import axios from "axios"; 
+let pokemon = axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+console.log(pokemon);
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https:/api.github.com/users/smarsden007')
+.then(resp => {
+  console.log(resp.data);
+})
+.catch(err => console.error(err))
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -29,6 +36,23 @@
 */
 
 const followersArray = [];
+
+
+function githubCard(gitInfo) {
+  const card = document.createElement('div');
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileLink = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+}
+
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
